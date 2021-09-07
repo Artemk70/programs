4f0de349445e5f947b5b020babd7a7006a167986
Кот Артем <artem70107@gmail.com>
2021-09-07 21:48:03 +0300

    Company application added

---
+++ company/src/Company/Operator.java
@@ -0,0 +1,12 @@
+package Company;
+
+public class Operator extends Staff {
+
+    private int monthSalary = 70000; // месячный оклад
+    private static int incomeCompany = 0; // доход компании
+
+    public int getMonthSalary() {
+        return monthSalary;
+    }
+
+}
